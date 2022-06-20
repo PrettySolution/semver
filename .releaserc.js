1cea40587290d83767c78d62d024e9eb2300de19
VH🔥 <vherman@vf-soft.com>
2022-06-20 23:44:59 +0300

    feat: gooo

--- .releaserc.js
+++ .releaserc.js
@@ -3,18 +3,19 @@ module.exports = {
         [
             "@semantic-release/commit-analyzer"
         ],
-        ["semantic-release-plugin-update-version-in-files", {
-            "files": [
-                "cortex-app/Chart.yaml"
-            ]
-        }],
+        [
+            "semantic-release-plugin-update-version-in-files",
+            {
+                "files": ["cortex-app/Chart.yaml"]
+            }
+        ],
         [
             "@semantic-release/exec",
             {
                 "prepareCmd": "scripts/prepare.sh",
                 "successCmd": "scripts/success.sh ${nextRelease.version}"
             }
-        ]
+        ],
         [
             "@semantic-release/release-notes-generator"
         ],
