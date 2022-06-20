be7fe84ae243f31326add31485094960278127d5
VH🔥 <vherman@vf-soft.com>
2022-06-20 23:08:17 +0300

    feat: aws

---
+++ images-.releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer"
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "scripts/prepare.sh ${process.env.DOCKER_USERNAME}/${process.env.DOCKER_REPOSITORY_NAME}",
+        "successCmd": "scripts/success.sh ${process.env.DOCKER_USERNAME}/${process.env.DOCKER_REPOSITORY_NAME}:${nextRelease.version}"
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator"
+    ],
+    [
+      "@semantic-release/github"
+    ]
+  ]
+}
